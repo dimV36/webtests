a4f2639814131756ba2310d28489e35c5b545aa1
dimv36 <carriingfate92@yandex.ru>
2014-11-25 23:46:12 +0300

    SQL scripts

---
+++ pgsql/cleardb.sql
@@ -0,0 +1,7 @@
+DROP TABLE application_data CASCADE;
+DROP TABLE investment_levels CASCADE;
+DROP TABLE processes CASCADE;
+DROP TABLE questions CASCADE;
+DROP TABLE roles CASCADE;
+DROP TABLE user_choices CASCADE;
+DROP TABLE users CASCADE;
