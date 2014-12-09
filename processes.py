b43dc7731b8d0726bacb5fb2aed5d1343119423c
dimv36 <carriingfate92@yandex.ru>
2014-12-09 09:32:05 +0300

    update process script

--- processes.py
+++ processes.py
@@ -8,20 +8,20 @@ if __name__ == '__main__':
         exit(1)
     out_file = open(argv[1] + '.sql', 'w')
     with open(argv[1]) as process_file:
-        process_id = 1
         sql = str()
         for process in process_file.readlines():
             process = process.rstrip('\n')
-            print(process)
+            role_id = int()
             if process.startswith('SSP'):
-                sql = u"INSERT INTO processes VALUES(%d, '%s', 3);" % (process_id, process.decode('utf-8'))
-            if process.startswith('GP'):
-                sql = u"INSERT INTO processes VALUES(%d, '%s', 4);" % (process_id, process.decode('utf-8'))
-            if process.startswith('OSP'):
-                sql = u"INSERT INTO processes VALUES(%d, '%s', 5);" % (process_id, process.decode('utf-8'))
-            if process.startswith('TSP'):
-                sql = u"INSERT INTO processes VALUES(%d, '%s', 6);" % (process_id, process.decode('utf-8'))
-            process_id += 1
+                role_id = 3
+            elif process.startswith('GP'):
+                role_id = 4
+            elif process.startswith('OSP'):
+                role_id = 5
+            elif process.startswith('TSP'):
+                role_id = 6
+            sql = u"INSERT INTO processes(id, name, role_id) VALUES(DEFAULT, '%s', %d) RETURNING id;" \
+                  % (process.decode('utf-8'), role_id)
             out_file.write(sql.encode('utf-8') + '\n')
         out_file.close()
 
