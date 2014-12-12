12ba18349888ad3b8c33ea3634fbd84b5cfd967c
dimv36 <carriingfate92@yandex.ru>
2014-12-12 12:27:49 +0300

    modify questions.py

--- questions.py
+++ questions.py
@@ -5,11 +5,10 @@ from sys import argv
 from webtests.models import Process
 
 if __name__ == '__main__':
-    # if len(argv) != 2:
-    #     print('Too few arguments for script')
-    #     exit(1)
-    # file_name = argv[1]
-    file_name = 'files/questions'
+    if len(argv) != 2:
+        print('Too few arguments for script')
+        exit(1)
+    file_name = argv[1]
     out_file = open(file_name + '.sql', 'w')
     with open(file_name) as questions_file:
         sql = str()
@@ -31,36 +30,34 @@ if __name__ == '__main__':
             # Парсим варианты ответа
             if question_name and line.startswith('-') and not found:
                 if not variants:
-                    variants = '{' + line.lstrip('-') + ', '
+                    variants = '\'{' + line.lstrip('-') + ', '
                 else:
                     variants += line.lstrip('-') + ', '
                 found = True
             # Парсим правильный вариант ответа
             if variants and line.startswith('+') and not found:
                 variants = variants[0:len(variants) - 2]
-                variants += '}'
+                variants += '}\''
                 correct_answer = line.lstrip('+')
+                if correct_answer == 'NULL':
+                    correct_answer = str(-1)
+                else:
+                    correct_answer = str(int(correct_answer) - 1)
                 found = True
             if correct_answer and line and not found:
                 metric = line
                 sql += u"INSERT INTO questions(id, name, variants, correct_answer, metric, process_id) VALUES" \
-                       u"(DEFAULT, '%s', %s, '%s', '%s', %s) RETURNING id;\n" \
-                  % (question_name.decode('utf-8'),
-                     variants.decode('utf-8'),
-                     correct_answer.decode('utf-8'),
-                     metric.decode('utf-8'),
-                     process_id)
-                # print('process_id: ' + str(process_id))
-                # print('question_name: ' + question_name)
-                # print('variants: ' + str(variants))
-                # print('correct: ' + correct_answer)
-                # print('metric: ' + metric)
+                       u"(DEFAULT, '%s', %s, %d, '%s', %d) RETURNING id;\n" % (question_name.decode('utf-8'),
+                                                                                 variants.decode('utf-8'),
+                                                                                 int(correct_answer),
+                                                                                 metric.decode('utf-8'),
+                                                                                 process_id)
                 question_name = str()
                 variants = str()
                 correct_answer = str()
                 metric = str()
             if not line:
-                sql += '--END OF process %d--\n\n' % process_id
+                # sql += '--END OF process %d--\n\n' % process_id
                 process_id = 0
             found = False
     out_file.write(sql.encode('utf-8'))
