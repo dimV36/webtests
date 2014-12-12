37059dba15f03e2034546490fc9e2b30508429b6
dimv36 <carriingfate92@yandex.ru>
2014-12-12 11:42:31 +0300

    script for create sql for questions table

--- questions.py
+++ questions.py
@@ -1,1 +1,67 @@
-__author__ = 'user'
+#!/usr/bin/python
+# coding=utf-8
+__author__ = 'dimv36'
+from sys import argv
+from webtests.models import Process
+
+if __name__ == '__main__':
+    # if len(argv) != 2:
+    #     print('Too few arguments for script')
+    #     exit(1)
+    # file_name = argv[1]
+    file_name = 'files/questions'
+    out_file = open(file_name + '.sql', 'w')
+    with open(file_name) as questions_file:
+        sql = str()
+        process_id = int()
+        question_name = str()
+        variants = str()
+        correct_answer = str()
+        metric = str()
+        found = False
+        for line in questions_file.readlines():
+            line = line.rstrip('\n')
+            if line.startswith('GP') or line.startswith('SSP') or line.startswith('TSP') or line.startswith('OSP'):
+                process_id = Process.query.filter(Process.name.like(line + '%')).one().id
+                found = True
+            # Парсим имя вопроса
+            if not process_id == 0 and not question_name and not found:
+                question_name = ' '.join(line.split(' ')[1:])
+                found = True
+            # Парсим варианты ответа
+            if question_name and line.startswith('-') and not found:
+                if not variants:
+                    variants = '{' + line.lstrip('-') + ', '
+                else:
+                    variants += line.lstrip('-') + ', '
+                found = True
+            # Парсим правильный вариант ответа
+            if variants and line.startswith('+') and not found:
+                variants = variants[0:len(variants) - 2]
+                variants += '}'
+                correct_answer = line.lstrip('+')
+                found = True
+            if correct_answer and line and not found:
+                metric = line
+                sql += u"INSERT INTO questions(id, name, variants, correct_answer, metric, process_id) VALUES" \
+                       u"(DEFAULT, '%s', %s, '%s', '%s', %s) RETURNING id;\n" \
+                  % (question_name.decode('utf-8'),
+                     variants.decode('utf-8'),
+                     correct_answer.decode('utf-8'),
+                     metric.decode('utf-8'),
+                     process_id)
+                # print('process_id: ' + str(process_id))
+                # print('question_name: ' + question_name)
+                # print('variants: ' + str(variants))
+                # print('correct: ' + correct_answer)
+                # print('metric: ' + metric)
+                question_name = str()
+                variants = str()
+                correct_answer = str()
+                metric = str()
+            if not line:
+                sql += '--END OF process %d--\n\n' % process_id
+                process_id = 0
+            found = False
+    out_file.write(sql.encode('utf-8'))
+    out_file.close()
